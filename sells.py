af694fb5a322cccdd4dd007ebe485c0c1da339ac
Matthew Arrow <mattarrow@gmail.com>
2018-01-09 11:25:17 -0500

    Update sells.py

--- sells.py
+++ sells.py
@@ -1,3 +1,3 @@
 from pyq import q, K
 
-q.set(':alpha', q('!', ["Name", "Ask", "Bid", "Last", "Vol", "Time"], [K.symbol([]), K.float([]), K.float([]), K.float([]), K.float([]), K.gtime([])]).flip)
+q.set(':alpha/HDB/', q('.Q.en', ':alpha', q('([]Name:`$(); Ask:`float$(); Bid:`float$(); Last:`float$(); Volume:`float$(); Time:`timestamp$())')))
